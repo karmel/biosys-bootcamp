d92d4933e44b79fb49e0f1301759ee2a34c4d6ae
Karmel Allison <karmel@arcaio.com>
2013-09-13 10:38:25 -0700

    Cleaning up for 2013 content

--- archives/2012/Kai/Kai_Zhang_Code/C.py
+++
@@ -1,3 +0,0 @@
-# In order to find the the most similar cell lines. I think we should use Cluster algorithmn. There is no easy way to do that by Python.
-
-# I was looking for something a little simpler than clustering, though that would be a good approach.
